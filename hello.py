a7a8ab9fb46d6c382c15109dd9cf3161f9b1253d
Joduro <Joduro@users.noreply.github.com>
2017-01-24 10:16:26 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2017 Github.com/Joduro, Joshua Charles Campbell, Eddie Antonio Santos
+# Copyright 2017 Github.com/Joduro, Eddie Antonio Santos
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
